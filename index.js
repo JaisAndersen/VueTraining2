38b1a8ec1e176f00efbada73bbb04476792461e6
Jais Andersen <jais.h.sorensen@gmail.com>
2025-03-14 12:58:27 +0100

    Play

---
+++ index.js
@@ -0,0 +1,18 @@
+Vue.createApp({
+    data() {
+        return {
+            name: "Jais",
+            age: 40,
+            atWork: true,
+            book: {
+                title: "MyBook",
+                price: 9.85
+            }
+        }
+    },
+    methods: {
+        save(word) {
+           
+        }
+    }
+}).mount("#app")
